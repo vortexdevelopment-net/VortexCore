2ef972be8985996e3930f0fe6be77a8e32d21d72
ceze88 <dev.ceze@gmail.com>
2025-04-25 11:09:42 +0200

    Add lang file check, if not present

--- src/main/java/net/vortexdevelopment/vortexcore/text/lang/Lang.java
+++ src/main/java/net/vortexdevelopment/vortexcore/text/lang/Lang.java
@@ -23,8 +23,8 @@ import java.util.stream.Stream;
 @RegisterReloadHook
 public class Lang implements ReloadHook {
 
+    private static boolean initialized = false;
     private static FileConfiguration lang;
-
     public static List<MiniMessagePlaceholder> staticPlaceholders = new ArrayList<>();
 
     private Lang() {
@@ -61,60 +61,103 @@ public class Lang implements ReloadHook {
     }
 
     public static String getString(String key) {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return key;
+        }
         return replaceStaticPlaceholders(lang.getString(key, key));
     }
 
     public static String getString(String key, String defaultValue) {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return defaultValue;
+        }
         return replaceStaticPlaceholders(lang.getString(key, defaultValue));
     }
 
     public static Component getComponent(String key) {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return AdventureUtils.formatComponent(key);
+        }
         return AdventureUtils.formatComponent(lang.getString(key, key), staticPlaceholders);
     }
 
     public static Component getComponent(String key, MiniMessagePlaceholder... placeholders) {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return AdventureUtils.formatComponent(key);
+        }
         return AdventureUtils.formatComponent(lang.getString(key, key), createPlaceholders(placeholders));
     }
 
     public static List<String> getList(String key) {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return List.of(key);
+        }
         return lang.getStringList(key);
     }
 
     public static void send(CommandSender player, String key) {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return;
+        }
         AdventureUtils.sendMessage(getComponent(key), player);
     }
 
     public static void sendPrefixed(CommandSender player, String key) {
-        AdventureUtils.sendMessage(getComponent(key), player);
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return;
+        }
+        AdventureUtils.sendMessage(getPrefix().append(getComponent(key)), player);
     }
 
     public static void send(CommandSender player, String key, MiniMessagePlaceholder... placeholders) {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return;
+        }
         AdventureUtils.sendMessage(getComponent(key, placeholders), player);
     }
 
     private static String getPrefixString() {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return VortexPlugin.getInstance().getPrefixString();
+        }
         return lang.getString("General.Plugin Prefix", VortexPlugin.getInstance().getPrefixString());
     }
 
     public static Component getPrefix() {
+        if (!initialized) {
+            VortexPlugin.getInstance().getLogger().warning("Language file has not been initialized!");
+            return VortexPlugin.getInstance().getPrefixWithDash();
+        }
         return getComponent("General.Plugin Prefix");
     }
 
     @Override
     public void onReload() {
-        staticPlaceholders.clear();
-        File langFile = new File(VortexPlugin.getInstance().getDataFolder(), "lang.yml");
-        if (!langFile.exists()) {
-            VortexPlugin.getInstance().saveResource("lang.yml", false);
-        }
-        lang = YamlConfiguration.loadConfiguration(langFile);
-        Gui.BACK_BUTTON_NAME = getString("GUI.Back Button Name", "§cBack");
-
-        staticPlaceholders.add(new MiniMessagePlaceholder("prefix", lang.getString("General.Plugin Prefix", VortexPlugin.getInstance().getPrefixString())));
-        if (lang.getConfigurationSection("Colors") != null) {
-            for (String placeholder : lang.createSection("Colors").getKeys(false)) {
-                staticPlaceholders.add(new MiniMessagePlaceholder(placeholder, "<color:" + lang.getString("Colors." + placeholder) + ">"));
+        try {
+            staticPlaceholders.clear();
+            File langFile = new File(VortexPlugin.getInstance().getDataFolder(), "lang.yml");
+            if (!langFile.exists()) {
+                VortexPlugin.getInstance().saveResource("lang.yml", false);
             }
-        }
+            lang = YamlConfiguration.loadConfiguration(langFile);
+            Gui.BACK_BUTTON_NAME = getString("GUI.Back Button Name", "§cBack");
+
+            staticPlaceholders.add(new MiniMessagePlaceholder("prefix", lang.getString("General.Plugin Prefix", VortexPlugin.getInstance().getPrefixString())));
+            if (lang.getConfigurationSection("Colors") != null) {
+                for (String placeholder : lang.createSection("Colors").getKeys(false)) {
+                    staticPlaceholders.add(new MiniMessagePlaceholder(placeholder, "<color:" + lang.getString("Colors." + placeholder) + ">"));
+                }
+            }
+            initialized = true;
+        } catch (Exception ignored) {}
     }
 }
