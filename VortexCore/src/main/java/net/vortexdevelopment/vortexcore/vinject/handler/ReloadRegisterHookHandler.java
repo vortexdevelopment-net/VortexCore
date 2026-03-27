d35261fed2cc002543cc1d2f868e81410e5a2dc8
ceze88 <dev.ceze@gmail.com>
2026-03-27 11:13:08 +0100

    Don't require ReloadHook interface for config classes

--- VortexCore/src/main/java/net/vortexdevelopment/vortexcore/vinject/handler/ReloadRegisterHookHandler.java
+++ VortexCore/src/main/java/net/vortexdevelopment/vortexcore/vinject/handler/ReloadRegisterHookHandler.java
@@ -1,6 +1,9 @@
 package net.vortexdevelopment.vortexcore.vinject.handler;
 
 import net.vortexdevelopment.vinject.annotation.component.Registry;
+import net.vortexdevelopment.vinject.annotation.yaml.YamlConfiguration;
+import net.vortexdevelopment.vinject.annotation.yaml.YamlDirectory;
+import net.vortexdevelopment.vinject.di.ConfigurationContainer;
 import net.vortexdevelopment.vinject.di.DependencyContainer;
 import net.vortexdevelopment.vinject.di.registry.AnnotationHandler;
 import net.vortexdevelopment.vortexcore.VortexPlugin;
@@ -12,12 +15,24 @@ public class ReloadRegisterHookHandler extends AnnotationHandler {
 
     @Override
     public void handle(Class<?> aClass, Object component, DependencyContainer dependencyContainer) {
-        //Check if the class is a org.bukkit.event.Listener
+        //Check if the class is a ReloadHook
         if (ReloadHook.class.isAssignableFrom(aClass)) {
             Object instance = component != null ? component : dependencyContainer.newInstance(aClass);
 
             //Register the listener
             VortexPlugin.getInstance().registerReloadHook((ReloadHook) instance);
+        }
+
+        // For configs auto register the reload hook
+        if (aClass.isAnnotationPresent(YamlDirectory.class) || aClass.isAnnotationPresent(YamlConfiguration.class)) {
+            ReloadHook reloadHook = new ReloadHook() {
+                @Override
+                public void onReload() {
+                    ConfigurationContainer.getInstance().reloadConfig(aClass);
+                }
+            };
+
+            VortexPlugin.getInstance().registerReloadHook(reloadHook);
         }
     }
 }
